19fb161a6bee14244e928aba97fac5c9516f888e
romainducrocq <romain.g.ducrocq@gmail.com>
2021-06-05 20:36:59 +0200

    make

--- bin/make.sh
+++ bin/make.sh
@@ -4,6 +4,8 @@ cd ..
 
 sudo apt-get update && sudo apt-get install build-essential libpq-dev libssl-dev openssl libffi-dev sqlite3 libsqlite3-dev libbz2-dev zlib1g-dev cmake python3.7 python3-pip python3.7-dev python3.7-venv
 
+mkdir venv && python3.7 -m venv venv/
+
 source venv/bin/activate
 
 pip3 install 'pyglet==1.5.0' gym torch tensorboard 'msgpack==1.0.2' wheel --no-cache-dir
